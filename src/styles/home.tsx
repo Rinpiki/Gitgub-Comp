063cd35132cffa1f518e870dac31658d73047831
Marcos Pablo <rinpikibr@gmail.com>
2023-03-28 07:55:30 -0300

    test: test vercel

---
+++ src/styles/home.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ContainerHome = styled.div`
+  display: flex;
+  width: 100vw;
+  max-width: 1300px;
+  flex-direction: column;
+  align-items: center;
+`
+export const Header = styled.div`
+  width: 100%;
+  padding: 25px 0px;
+  align-items: flex-start;
+  font-size: 14px;
+`
